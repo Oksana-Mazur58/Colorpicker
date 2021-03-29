fe5a9912b3bb35544014023e6864d76178262262
Oksana-Mazur58 <72046543+Oksana-Mazur58@users.noreply.github.com>
2021-03-29 23:43:23 +0300

    створила сolorpicker

---
+++ js/colorpicker.js
@@ -0,0 +1,44 @@
+import listColorsTemp from '../templates/color-card.hbs'
+import colorsData from './colors.json';
+// import './style.css';
+const paletteContainer = document.querySelector('.js-palette');
+const cardMarkp = listColorsTemp(colorsData);
+paletteContainer.insertAdjacentHTML('beforeend', cardMarkp);
+
+
+paletteContainer.addEventListener('click', OnPaletteContainerClick)
+
+function OnPaletteContainerClick(evn) {
+    if (!evn.target.classList.contains('color-swatch')) {
+        return
+    }
+    //вибираємо найближчий div вверх в який включає клас .color-card для елемента на який натиснули 
+    const parentColorCard = evn.target.closest('.color-card');
+    console.log(parentColorCard)
+
+    // функція яка шукає картку в якої вже є клас .is-active і видаляє його
+    removeActiveCardClass() 
+
+    //функція яка додає активний клас на вибрану картку
+    addActiveClass(parentColorCard)
+
+    // функція яка витягує з активного елемента hex і додаємо його на body
+    setBodyBgColor(evn.target.dataset.hex);
+    
+}
+function setBodyBgColor(color) {
+    document.body.style.background = color
+}
+function removeActiveCardClass() {
+    // шукаємо картку в якої вже є клас .is-active
+    const currentActiveCard = document.querySelector('.color-card.is-active');
+    //провіряємо якщо є така карточка видаляємо з неї такий клас
+    if (currentActiveCard) {
+        currentActiveCard.classList.remove('is-active')
+    }   
+}
+
+function addActiveClass(сard) {
+    //на цей елемент додаємо клас is-active
+    сard.classList.add('is-active') 
+}
